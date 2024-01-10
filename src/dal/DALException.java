f00decdb4d75f7249400f7076f698953834111c8
Pierre-Olivier M <po.mpolesha@gmail.com>
2024-01-10 16:02:00 +0100

    ScheduleDAO

--- src/dal/DALException.java
+++ src/dal/DALException.java
@@ -1,8 +1,13 @@
 package dal;
 
 public class DALException extends Exception {
-
-	
 	private static final long serialVersionUID = -2155716241313409287L;
+
+
+	public DALException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	
+	
 
 }
