641ee6d1a2676b74b8a468c33041f0e883338a61
unknown <nathan.hamon@3wa.io>
2024-01-10 12:28:58 +0100

    maj du set up du projet

---
+++ src/bll/BLLException.java
@@ -0,0 +1,8 @@
+package bll;
+
+public class BLLException extends Exception {
+
+
+	private static final long serialVersionUID = -8402728185430838053L;
+
+}
--- src/dal/GenericDAOInterface.java
+++ src/dal/GenericDAOInterface.java
@@ -1,6 +1,18 @@
 package dal;
 
+import java.util.List;
+
 public interface GenericDAOInterface<T> 
 {
+	
+	List<T> selectAll() throws DALException;
+	
+	T selectById(int id) throws DALException;
+	
+	void insert(T objet) throws DALException;
+	
+	void update(T objet) throws DALException;
+	
+	void delete(T objet) throws DALException;
 
 }
